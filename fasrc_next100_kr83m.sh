80aa9c60c300096ee62be41312960cd70ae14295
Justo Martín-Albo <jmalbos@users.noreply.github.com>
2020-06-03 04:45:09 -0400

    Cosmetics

--- fasrc_next100_kr83m.sh
+++ fasrc_next100_kr83m.sh
@@ -4,16 +4,17 @@
 #SBATCH -n 1            # Number of cores
 #SBATCH -N 1            # All cores on one machine
 #SBATCH -p guenette     # Partition
-#SBATCH --mem 500       # Memory request (Mb)
-#SBATCH -t 0-03:00      # Maximum execution time (D-HH:MM)
+#SBATCH --mem 1000      # Memory request (Mb)
+#SBATCH -t 0-24:00      # Maximum execution time (D-HH:MM)
 #SBATCH -o %A_%a.out    # Standard output
 #SBATCH -e %A_%a.err    # Standard error
 
 ## Job options
 RNDSEED=${SLURM_ARRAY_TASK_ID}
+PRODNUM=20200529
 NEVENTS=1000
 STARTID=$(((RNDSEED-1)*NEVENTS))
-OUTFILE="/n/holyscratch01/guenette_lab/jmartinalbo/next100.kr83m.nexus.${SLURM_ARRAY_TASK_ID}"
+OUTFILE="/n/holyscratch01/guenette_lab/jmartinalbo/next100.kr83m.${SLURM_ARRAY_TASK_ID}.nexus"
 
 ## Configure scisoft software products
 . /n/holystore01/LABS/guenette_lab/Lab/software/next/scisoft/setup
@@ -22,8 +23,8 @@ setup root   v6_18_04    -q e19:prof
 setup geant4 v4_10_6_p01 -q e19:prof
 
 ## Generate nexus macros
-INI_MACRO="/n/holyscratch01/guenette_lab/jmartinalbo/nexus.init.${SLURM_ARRAY_TASK_ID}.mac"
-CFG_MACRO="/n/holyscratch01/guenette_lab/jmartinalbo/nexus.config.${SLURM_ARRAY_TASK_ID}.mac"
+INI_MACRO="/n/holyscratch01/guenette_lab/jmartinalbo/${PRODNUM}.${SLURM_ARRAY_TASK_ID}.init.mac"
+CFG_MACRO="/n/holyscratch01/guenette_lab/jmartinalbo/${PRODNUM}.${SLURM_ARRAY_TASK_ID}.config.mac"
 
 echo "/PhysicsList/RegisterPhysics G4EmStandardPhysics_option4" >> ${INI_MACRO}
 echo "/PhysicsList/RegisterPhysics G4DecayPhysics"              >> ${INI_MACRO}
@@ -45,14 +46,14 @@ echo "/tracking/verbose 0"                                      >> ${CFG_MACRO}
 echo "/Generator/Kr83mGenerator/region ACTIVE"                  >> ${CFG_MACRO}
 echo "/Geometry/Next100/pressure 15.0 bar"                      >> ${CFG_MACRO}
 echo "/Geometry/Next100/sc_yield 25510. 1/MeV"                  >> ${CFG_MACRO}
-echo "/Geometry/Next100/EL_field 20. kilovolt/cm"               >> ${CFG_MACRO}
+echo "/Geometry/Next100/EL_field 16. kilovolt/cm"               >> ${CFG_MACRO}
 echo "/Geometry/Next100/drift_transv_diff 1.2 mm/sqrt(cm)"      >> ${CFG_MACRO}
 echo "/Geometry/Next100/drift_long_diff   0.3 mm/sqrt(cm)"      >> ${CFG_MACRO}
 echo "/Geometry/Next100/e_lifetime 12.0 ms"                     >> ${CFG_MACRO}
 echo "/Geometry/Next100/elfield true"                           >> ${CFG_MACRO}
 echo "/Geometry/Next100/max_step_size 1. mm"                    >> ${CFG_MACRO}
 echo "/Geometry/Next100/sipm_time_binning 1. microsecond"       >> ${CFG_MACRO}
-echo "/Geometry/PmtR11410/time_binning 1. nanosecond"           >> ${CFG_MACRO}
+echo "/Geometry/PmtR11410/time_binning 25. nanosecond"          >> ${CFG_MACRO}
 echo "/Geometry/PmtR11410/SD_depth 3"                           >> ${CFG_MACRO}
 echo "/PhysicsList/Nexus/clustering          true"              >> ${CFG_MACRO}
 echo "/PhysicsList/Nexus/drift               true"              >> ${CFG_MACRO}
@@ -62,5 +63,5 @@ echo "/nexus/persistency/start_id ${STARTID}"                   >> ${CFG_MACRO}
 echo "/nexus/persistency/outputFile ${OUTFILE}"                 >> ${CFG_MACRO}
 echo " "                                                        >> ${CFG_MACRO}
 
-/n/holystore01/LABS/guenette_lab/Lab/software/next/nexus/nexus-v6_00_00/build/source/nexus -b -n ${NEVENTS} ${INI_MACRO}
-cp "${OUTFILE}.h5" /n/holystore01/LABS/guenette_lab/Lab/data/NEXT/NEXT100/sim/kr83m/20200525/nexus
+time /n/holystore01/LABS/guenette_lab/Lab/software/next/nexus/nexus-v6_00_00/build/source/nexus -b -n ${NEVENTS} ${INI_MACRO}
+cp "${OUTFILE}.h5" /n/holystore01/LABS/guenette_lab/Lab/data/NEXT/NEXT100/sim/kr83m/${PRODNUM}/nexus
